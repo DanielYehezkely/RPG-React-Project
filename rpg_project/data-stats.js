81ee87bcbbe056c39fa79f9a859dc78744b7008f
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-07 22:51:17 +0300

    data-stats

---
+++ rpg_project/data-stats.js
@@ -0,0 +1,11 @@
+const dataStats = [
+{ name: 'Mr. SKINNER', vector: '265', hp: '98'},
+{ value: '3:48', type: 'LONGEST LEVEL' },
+{ value: '5.7k', type: 'DAMAGE DONE' },
+{ value: '487', type: 'TOTAL KILLS' },
+{ value: '1/8', type: 'MAPS COMPLETE' },
+{ value: '8/11', type: 'SQUADDIES OWNED' },
+{ value: '2.3k', type: 'DAMAGE RECEIVED' },
+{ value: '109', type: 'TOTAL DEATHS' },
+{ value: '2', type: 'Events WON' },
+]
