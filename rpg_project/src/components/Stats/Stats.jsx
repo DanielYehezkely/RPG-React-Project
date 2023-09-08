8da3b3e6c70060b3550fd4e8f55576fb2f24bf20
brainstromsurfer <wishmeheaven@gmail.com>
2023-09-08 23:13:50 +0300

    stats-items-component_complete

--- rpg_project/src/components/Stats/Stats.jsx
+++ rpg_project/src/components/Stats/Stats.jsx
@@ -1,13 +1,40 @@
+const Stats = ({id, name, precision, kills, category, value}) => {
+  console.log("id:", id); // Log the value of id for debugging
 
-const Stats = () => {
-  return (
-    <div className='stats-item-box'>
-      <div className="stats-item-data">
-        3:48
+  return (    
+    <>
+   { id === 'state-item-0' ? (
+     <div className='stats-item-box'> 
+     <div className="stats-item-first-box-inner">
+     <h4 className="favorite-squaddie-name">{name}</h4>    
+     <div className="favorite-squaddie-values-container">
+      <div className="favorite-squaddie-icon-precision"></div>
+      <p>{precision}</p>
+        <div className="favorite-squaddie-icon-kills"></div>
+        <p>{kills}</p>        
       </div>
-      <div className="stats-item-type">longest ride</div>
+      <div className="modal-stats-character-image">
+      <div className="image-bald-survivor"></div>
+      </div>
+      </div>  
+      <div className="stats-item-category"> {category} </div>
+     </div> ) : (
+     <div className='stats-item-box'>   
+         <div className="stats-item-value"> {value} </div>
+         <div className="stats-item-category"> {category} </div>
     </div>
+    )}
+    </>
   )
 }
 
 export default Stats
+/*
+
+   {props.id === 'state-item-0'} ?
+      <div></div>
+      { id: 'state-item-0' , name: 'MR. SKINNER', precision: 265, kills: 98 , category: 'FAVORITE SQUADDIE'},
+      <div className="stats-item-data favorite-squaddie">{props.name}{props.name}</div> :
+      <div className="stats-item-type">{props.value} {props.category}</div>
+
+**/
--- rpg_project/src/components/Stats/StatsContainer.jsx
+++ rpg_project/src/components/Stats/StatsContainer.jsx
@@ -1,9 +1,15 @@
+import { statsItems } from "../../utils/stats-items.js"
 import Stats from "./stats";
-import stateItems from "../../utils/stats-items"
 const StatsContainer = () => {
     return (
         <div className="modal-stats-items-container">
-            <Stats />
+            {statsItems.map((item, index) => (
+                index === 0 ?
+                <Stats key={item.id} id={item.id} name={item.name} precision={item.precision} kills={item.kills} category={item.category} />
+                 :
+                <Stats key={item.id} value={item.value} category={item.category} />
+                )  
+            )}            
         </div>
     )
 }
